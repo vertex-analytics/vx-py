d796850a1128a64eca18ea262936ab593d4c3ca9
z2048 <37806310+z2048@users.noreply.github.com>
2024-02-21 17:43:12 -0600

    Add files via upload

---
+++ demo_buckets.py
@@ -0,0 +1,106 @@
+from vx_enum import *
+
+import vx_bind as  vx
+
+from datetime import datetime
+import time
+
+class MyFeed(vx.feed):
+	
+	def __init__(self):
+		super(MyFeed, self).__init__()
+		self.fBuckRows	=	0
+		
+	def onInit(self,pData):
+		print ('onInit')
+		print (pData)
+
+	def onConnect(self,pData):
+		print ('onConnect')
+
+	def onExit(self,pData):
+		print ('onExit')
+
+	def onOpen(self,pData):
+		print ('onOpen')
+		print (pData)
+
+	def onShut(self,pData):
+		print ('onShut')
+		print ("Total Buckets {}".format(self.fBuckRows))
+
+	#def onEvent(self,pData,pEvent):
+		#print ('onEvent')
+		
+	#def onProgress(self,pData):
+		# print ('onProgress')
+		# print (pData)
+
+	def onError(self,pData):
+		#print ('onError')
+		print (pData)
+
+	def onWarning(self,pData):
+		#print ('onWarning')	
+		print (pData)
+	
+	def onMessage(self,pData):
+		print ('onMessage')
+		print (pData)
+
+	def onBucket(self,pData):
+		
+		tBucket = pData["bucket"]	
+			
+		if (tBucket==Bucket.Trades)	:
+			
+			tTime = datetime.fromtimestamp (pData['time']/1000);
+			tDate = tTime.isoformat(sep=' ', timespec='milliseconds')
+			print(tDate)
+		
+			print(	"Trades: "
+					"time:%(time)s,"
+					"total:%(total)d,"
+					"volume:%(volume)d,"
+					"high:%(high)5.2f,"
+					"low:%(low)5.2f,"
+					"aggSellers:%(aggSellers)d,"
+					"aggBuyers:%(aggBuyers)d,"
+					"midPoint:%(midPoint)5.2f,"
+					"midPoint_above:%(midPoint_above)d,"
+					"midPoint_below:%(midPoint_below)d,"
+					"vwap:%(vwap)5.2f,"
+					"vwap_above:%(vwap_above)d,"
+					"vwap_below:%(vwap_below)d"
+					%pData) 
+
+	
+		self.fBuckRows	=	self.fBuckRows+1
+		
+	#def onIdle(self,pData):
+		#print ('onIdle')
+		#print (pData)
+
+def test():
+
+	myFeed = MyFeed()
+
+	myFeed.request ({
+		'email'				:	'xx@zz.com',
+		'password'			:	'password',		
+		'symbol'			:	'ZS',		
+		'startDate'			:	20230501,
+		'endDate'			:	20230502,
+		'buckets'			:	Bucket.Trades,
+		'interval'			:	1000*60*60,	#one hour
+		'weekends'			:	False,
+		'realtime'			:	True,
+		'fids'				:	'kFidsTrds',
+		'cachePath'			:	'E:\\_vx',
+		'cacheRead'			:	True	
+	})
+	
+	
+if __name__ == "__main__":   
+	test()
+	
---
+++ demo_summary.py
@@ -0,0 +1,99 @@
+from vx_enum import *
+
+import vx_bind as  vx
+
+class MyFeed(vx.feed):
+	
+	def __init__(self):
+		super(MyFeed, self).__init__()
+		self.fTrigRows	=	0
+		
+	def onInit(self,pData):
+		print ('onInit')
+		print (pData)
+
+	def onConnect(self,pData):
+		print ('onConnect')
+
+	def onExit(self,pData):
+		print ('onExit')
+
+	def onOpen(self,pData):
+		print ('onOpen')
+		print (pData)
+		self.fEvntRows	=	0
+
+	def onShut(self,pData):
+		print ('onShut')
+		print("Total Triggers {}".format(self.fTrigRows))
+
+	#def onEvent(self,pData,pEvent):
+		#print ('onEvent')
+		
+	def onProgress(self,pData):
+		print ('onProgress')
+		print (pData)
+
+	def onError(self,pData):
+		#print ('onError')
+		print (pData)
+
+	def onWarning(self,pData):
+		self.fEvntRows	=	0
+		#print ('onWarning')	
+		print (pData)
+	
+	def onMessage(self,pData):
+		print ('onMessage')
+		print (pData)
+
+	def onTrigger(self,pData):
+		
+		#print ('onTrigger')
+		#print (pData)
+
+		tTrigger = pData["trigger"]	
+			
+		if (tTrigger==Trigger.Summary)	:
+			if self.fTrigRows < 100	:
+				print(	"Summary: "
+						"time:%(time)d,"
+						"aggressor:%(aggressor)d,"
+						"quantity:%(quantity)d,"
+						"orders:%(orders)d,"
+						"price:%(price)5.2f,"
+						"volume:%(volume)d,"
+						"vwap:%(vwap)5.2f,"
+						"px2vwap_min:%(px2vwap_min)5.2f,"
+						"px2vwap_max:%(px2vwap_max)5.2f"
+						"px2vwap_time:%(px2vwap_time)5.2f"
+						%pData) 
+			
+			self.fTrigRows	=	self.fTrigRows+1
+		
+	#def onIdle(self,pData):
+		#print ('onIdle')
+		#print (pData)
+
+def test():
+
+	myFeed = MyFeed()
+
+	myFeed.request ({
+		'email'				:	'xx@zz.com',
+		'password'			:	'password',		
+		'symbol'			:	'ZS',		
+		'startDate'			:	20240216,
+		'endDate'			:	20240216,
+		'triggers'			:	Trigger.Summary,
+		'weekends'			:	False,
+		'realtime'			:	True,
+		'fids'				:	'kFidsTrds',
+		'cachePath'			:	'E:\\_vx',
+		'cacheRead'			:	True	
+	})
+	
+	
+if __name__ == "__main__":   
+	test()
+	
